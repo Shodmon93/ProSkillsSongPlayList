356aca9114b39b3d182f07a7a210e1e01bf9b975
Shodmon <shodik.9393@gmail.com>
2023-01-22 22:37:32 +0500

    cleaned up the code

--- app/src/main/java/com/example/data/PlaylistData.kt
+++ app/src/main/java/com/example/data/PlaylistData.kt
@@ -5,6 +5,5 @@ import androidx.recyclerview.widget.RecyclerView
 data class PlaylistData(
     val id :Int,
     val name : String? = null,
-    val recyclerView: RecyclerView? = null,
-    val songData: SongData? =null
+    val recyclerView: RecyclerView? = null
 )
